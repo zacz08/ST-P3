82c9b2ee7a8afab64d99f0293afa6fb84b8a0e10
zacz08 <zacharyzhang95@163.com>
2024-10-15 22:48:46 +0100

    change np.int to int to fit numpy >= 1.20

--- stp3/models/encoder.py
+++ stp3/models/encoder.py
@@ -25,7 +25,7 @@ class Encoder(nn.Module):
             raise NotImplementedError
         self.upsampling_out_channel = [0, 48, 64, 128, 512]
 
-        index = np.log2(self.downsample).astype(np.int)
+        index = np.log2(self.downsample).astype(int)
 
         if self.use_depth_distribution:
             self.depth_layer_1 = DeepLabHead(self.reduction_channel[index+1], self.reduction_channel[index+1], hidden_channel=64)
@@ -81,7 +81,7 @@ class Encoder(nn.Module):
         # Head
         endpoints['reduction_{}'.format(len(endpoints) + 1)] = x
 
-        index = np.log2(self.downsample).astype(np.int)
+        index = np.log2(self.downsample).astype(int)
         input_1 = endpoints['reduction_{}'.format(index + 1)]
         input_2 = endpoints['reduction_{}'.format(index)]
 
